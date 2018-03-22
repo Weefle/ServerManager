fd514ef46fcadaf3fdef58684a28066bfb17a30f
Weefle <mr.weefle@gmail.com>
2018-03-22 21:22:56 +0100

    Update

--- src/fr/weefle/servermanager/servers/FilesUtils.java
+++ src/fr/weefle/servermanager/servers/FilesUtils.java
@@ -17,6 +17,7 @@ public class FilesUtils {
                     e.printStackTrace();
                 }
             });
+            
         } catch (IOException e) {
             e.printStackTrace();
         }
